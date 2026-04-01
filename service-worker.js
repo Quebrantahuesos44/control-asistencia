60ce72a806b5419f33c33764694b431b4cc26f1d
Quebrantahuesos44 <daniojeda2006@gmail.com>
2026-04-01 09:24:21 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('evento-cache').then(cache => {
       return cache.addAll([
-        'control_evento_pro.html'
+        'index.html'
       ]);
     })
   );
